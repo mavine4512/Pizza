fde020f31d361e6d15e5e8342fe5055664d5f90f
Mavine Naaman <43817772+mavine4512@users.noreply.github.com>
2020-05-05 09:59:42 +0300

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -1,7 +1,7 @@
 export const storeProducts = [
   {
-    id: 1,
-    title: "Caramill flevar",
+   id: 1,
+    title: "Caramel flavour",
     img: "img/pizza1.jpg",
     price: 27,
     company: "namTech",
@@ -12,8 +12,8 @@ export const storeProducts = [
     total: 0,
   },
   {
-    id: 2,
-    title: "Chilly hot flevar",
+   id: 2,
+    title: "Chilly hot flavour",
     img: "img/pizza2.jpg",
     price: 31,
     company: "namTech",
@@ -25,7 +25,7 @@ export const storeProducts = [
   },
   {
     id: 3,
-    title: "Pork flevar",
+    title: "Pork flavour",
     img: "img/pizza3.jpg",
     price: 21,
     company: "namTech",
@@ -37,7 +37,7 @@ export const storeProducts = [
   },
   {
     id: 4,
-    title: "Chicken flevar",
+    title: "Chicken flavour",
     img: "img/pizza4.jpg",
     price: 75,
     company: "namTech",
@@ -49,7 +49,7 @@ export const storeProducts = [
   },
   {
     id: 5,
-    title: "Banana flevar",
+    title: "Banana flavour",
     img: "img/pizza5.jpg",
     price: 21,
     company: "namTech",
@@ -61,7 +61,7 @@ export const storeProducts = [
   },
   {
     id: 6,
-    title: "Strobery flevar",
+    title: "strawberry flavour",
     img: "img/pizza6.jpg",
     price: 50,
     company: "namTech",
@@ -73,7 +73,7 @@ export const storeProducts = [
   },
   {
     id: 7,
-    title: "Black berry flevar",
+   title: "Black berry flavour",
     img: "img/pizza7.jpg",
     price: 10,
     company: "namTech",
@@ -85,7 +85,7 @@ export const storeProducts = [
   },
   {
     id: 8,
-    title: "Vegetable flevar",
+     title: "Vegetable flavour",
     img: "img/pizza8.jpg",
     price: 15,
     company: "namTech",
@@ -98,14 +98,14 @@ export const storeProducts = [
 ];
 
 export const detailProduct = {
-  id: 1,
-  title: "Strobery flevar",
-  img: "img/pizza1.jpg",
-  price: 27,
-  company: "namTech",
-  description:
-    "When you are in charge of a team,lead by example.say 'we' more often than'i' but always understand that the buck stops with you. if something goes wrong, take responsibility without blaming others.when the right time comes, discuss the problems calmly with the team.",
-  inCart: false,
-  count: 0,
-  total: 0,
+ id: 1,
+    title: "Caramel flavour",
+    img: "img/pizza1.jpg",
+    price: 27,
+    company: "namTech",
+    description:
+      "When you are in charge of a team,lead by example.say 'we' more often than'i' but always understand that the buck stops with you. if something goes wrong, take responsibility without blaming others.when the right time comes, discuss the problems calmly with the team.",
+    inCart: false,
+    count: 0,
+    total: 0,
 };
